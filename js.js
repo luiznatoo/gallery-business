c6926c17e36346bbd0bc31abf5f8a8ea0053edec
Luiz Nonato <121554100+luiznatoo@users.noreply.github.com>
2023-03-17 11:16:38 -0300

    primeira versão do site

---
+++ js.js
@@ -0,0 +1,6 @@
+ScrollReveal({ reset: true});
+
+
+ScrollReveal().reveal('.move1', { delay: 500 });
+ScrollReveal().reveal('.move2', { delay: 600 });
+ScrollReveal().reveal('.move3', { delay: 700 });
